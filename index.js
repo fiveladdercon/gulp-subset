d57c6e87044fe67d8fd821112694b2c933c247b4
fiveladdercon <fiveladdercon@gmail.com>
2016-01-26 16:17:25 -0500

    Dropped stream flush

--- index.js
+++ index.js
@@ -17,26 +17,18 @@ module.exports = function (pattern,inner,invert)  {
 	if (!inner      ) throw new PluginError('gulp-subset','Missing stream operation');
 	if (!inner.write) throw new PluginError('gulp-subset','Stream operation does not appear to be writeable');
 
-	var outer = new Transform({ objectMode: true, transform: transform, flush: flush });
+	var outer = new Transform({ objectMode: true, transform: transform });
 
 	function transform (file, encoding, next) {
 		if (invert ? !matching(file) : !!matching(file)) {
-			inner.write(file,encoding,function () { next(); });
+			inner.write(file, encoding, function () { next(); });
 		} else {
 			next(null,file);
 		}
 	}
 
-	function flush (next) {	
-		outer.on('inner', function () { next();	});	
-	}
-
 	inner.on('data', function (file) { 
 		outer.push(file); 
-	});
-
-	inner.on('end', function () { 
-		outer.emit('inner'); 
 	});
 
 	return outer;
